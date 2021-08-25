ff56bac5f902f68187f5993b5ad0fbe632f28b33
Liêu Phúc Mai <56626332+LieuMai@users.noreply.github.com>
2021-08-25 18:31:20 +0700

    useContext, custom-hooks, prop-types, react-router

--- react-tutorial/src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ react-tutorial/src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,45 @@
 import React, { useState } from 'react';
-
+import { data } from '../../../data'
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people,setPeople] = useState(data);
+
+  // Create a remove function here
+  const removePerson = (id) => {
+    setPeople((people) => {
+      return people.filter((person) => person.id !== id);
+    })
+  }
+
+  return <section>
+    <h3>Prop Drilling</h3>
+    {/*//then pass it down here*/}
+    <List people={people} removePerson={removePerson} /> 
+  </section>;
 };
+
+const List = ({people,removePerson}) => { // and then here
+  return <>
+    {people.map((person) => {
+      return (
+        <SinglePerson key={person.id} {...person} removePerson={removePerson} /> // and here
+      )
+    })}
+  </>
+}
+
+const SinglePerson = ({id,name,removePerson}) => { // here too
+  return (
+    <div className="item">
+      <h4>{name}</h4>
+      <button className="btn" 
+        // finally here => prop drilling~~
+        onClick={()=>removePerson(id)}>remove
+      </button>
+    </div>
+  )
+}
 
 export default PropDrilling;
--- react-tutorial/src/tutorial/8-useContext/setup/1-context-api.js
+++ react-tutorial/src/tutorial/8-useContext/setup/1-context-api.js
@@ -3,6 +3,10 @@ import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+const PersonContext = React.createContext();
+// two components - Provider, Consumer
+
+
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
   const removePerson = (id) => {
@@ -10,23 +14,26 @@ const ContextAPI = () => {
       return people.filter((person) => person.id !== id);
     });
   };
+
+  // use PersonContext as a Provider tag, then pass all values to it
   return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+    <PersonContext.Provider value={{removePerson,people}}> 
+      <h3>context api</h3>
+      <List />
+    </PersonContext.Provider>
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = () => {
+  //const {people} = useContext(PersonContext)
+  const mainData = useContext(PersonContext) // then take it out to use
   return (
     <>
-      {people.map((person) => {
+      {mainData.people.map((person) => {
         return (
           <SinglePerson
             key={person.id}
             {...person}
-            removePerson={removePerson}
           />
         );
       })}
@@ -34,7 +41,8 @@ const List = ({ people, removePerson }) => {
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  const {removePerson} = useContext(PersonContext); // take it out again
   return (
     <div className='item'>
       <h4>{name}</h4>
